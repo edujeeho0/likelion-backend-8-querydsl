1c4ccd9d06aae4a63bdb6341bb529659c1f35647
edujeeho <edujeeho@gmail.com>
2024-02-06 13:17:09 +0900

    init

---
+++ src/main/java/com/example/querydsl/QuerydslApplication.java
@@ -0,0 +1,14 @@
+package com.example.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class QuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslApplication.class, args);
+	}
+
+}
